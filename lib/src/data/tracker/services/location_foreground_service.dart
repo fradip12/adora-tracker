79f25d5eaa17f4bde4ff1276c99c2f8ece5f14f6
fradip12 <dipodipo92@gmail.com>
2026-05-18 18:54:30 +0700

    feat: add LocationForegroundService static wrapper

---
+++ lib/src/data/tracker/services/location_foreground_service.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
+
+import '../../settings/enums/tracking_interval.dart';
+import '../workers/location_foreground_task.dart';
+
+class LocationForegroundService {
+  static void _configure(TrackingInterval interval) {
+    FlutterForegroundTask.init(
+      androidNotificationOptions: AndroidNotificationOptions(
+        channelId: 'adaro_tracking',
+        channelName: 'Adaro Tracking',
+        channelImportance: NotificationChannelImportance.LOW,
+        priority: NotificationPriority.LOW,
+      ),
+      iosNotificationOptions: const IOSNotificationOptions(),
+      foregroundTaskOptions: ForegroundTaskOptions(
+        eventAction:
+            ForegroundTaskEventAction.repeat(interval.duration.inMilliseconds),
+        autoRunOnBoot: false,
+      ),
+    );
+  }
+
+  static Future<ServiceRequestResult> start(
+    int sessionId,
+    TrackingInterval interval,
+  ) async {
+    _configure(interval);
+    await FlutterForegroundTask.saveData(
+      key: 'session_id',
+      value: sessionId.toString(),
+    );
+    return FlutterForegroundTask.startService(
+      serviceId: 256,
+      notificationTitle: 'Adaro',
+      notificationText: 'Location tracking is active',
+      callback: startCallback,
+    );
+  }
+
+  static Future<ServiceRequestResult> stop() =>
+      FlutterForegroundTask.stopService();
+}
