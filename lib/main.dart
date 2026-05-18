d83808a512e3507f2c345b0b259ce78a5fa188cf
fradip12 <dipodipo92@gmail.com>
2026-05-18 19:03:58 +0700

    chore: init flutter_foreground_task communication port in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 
 import 'app.dart';
 import 'src/core/config/app_di.dart';
@@ -10,13 +11,15 @@ import 'src/core/utils/debug_bloc_observer.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  FlutterForegroundTask.initCommunicationPort();
+
   await configureDependencies();
 
   Bloc.observer = MultiBlocObserver(
     observers: [if (kDebugMode) DebugBlocObserver()],
   );
 
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await SystemChrome.setPreferredOrientations([.portraitUp]);
 
   runApp(const AdoraApp());
 }
